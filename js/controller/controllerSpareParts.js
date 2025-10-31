2c862e327ed0c302f3bbc9984e62355433d7ab9a
cvcg11 <cristianvcg11@gmail.com>
2025-10-30 22:54:48 -0600

    Js en utils para abrir y cerrar modals

---
+++ js/controller/controllerSpareParts.js
@@ -0,0 +1,5 @@
+import {
+    setupModal
+} from '../utils.js';
+// Configurar el modal para agregar repuestos
+setupModal("#modalParts", "#modalSpareParts", "#closeAddEmployee");
--- js/controller/controllerVehicle.js
+++ js/controller/controllerVehicle.js
@@ -1,30 +1,36 @@
-    const ctx = document.getElementById('vehicleChart').getContext('2d');
+import {
+    setupModal
+} from '../utils.js';
+// Configurar el modal para agregar Vehículos
+setupModal("#OpenModalVehicles", "#modalVehicle", "#closeAddVehicle");
 
-    const data = {
-      labels: ['Vendido', 'En stock'],
-      datasets: [{
-        data: [50, 6], // 🔹 Puedes cambiar estos valores dinámicamente desde tu base de datos
-        backgroundColor: ['#D31813', '#E4B9B8'],
-        hoverOffset: 10,
-        borderWidth: 0
-      }]
-    };
+const ctx = document.getElementById('vehicleChart').getContext('2d');
 
-    new Chart(ctx, {
-      type: 'doughnut',
-      data: data,
-      options: {
-        responsive: true,
-        plugins: {
-          legend: { display: false },
-          tooltip: {
-            backgroundColor: '#fff',
-            titleColor: '#0D0503',
-            bodyColor: '#0D0503',
-            borderColor: '#CD908F',
-            borderWidth: 1
-          }
-        },
-        cutout: '70%',
+const data = {
+  labels: ['Vendido', 'En stock'],
+  datasets: [{
+    data: [50, 6], // 🔹 Puedes cambiar estos valores dinámicamente desde tu base de datos
+    backgroundColor: ['#D31813', '#E4B9B8'],
+    hoverOffset: 10,
+    borderWidth: 0
+  }]
+};
+
+new Chart(ctx, {
+  type: 'doughnut',
+  data: data,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: { display: false },
+      tooltip: {
+        backgroundColor: '#fff',
+        titleColor: '#0D0503',
+        bodyColor: '#0D0503',
+        borderColor: '#CD908F',
+        borderWidth: 1
       }
-    });
+    },
+    cutout: '70%',
+  }
+});
