146378d171083ba3aa5512c5bd96266e8304b3e7
Sherartsu <jose.merino9108@gmail.com>
2025-11-10 16:27:49 -0600

    mas cambios

--- js/controller/controllerSpareParts.js
+++ js/controller/controllerSpareParts.js
@@ -3,37 +3,3 @@ import {
 } from '../utils.js';
 // Configurar el modal para agregar repuestos
 setupModal("#modalParts", "#modalSpareParts", "#closeAddEmployee");
-
-const ctx = document.getElementById('idTotalCost').getContext('2d');
-
-const data = {
-    labels: ['Mecánica', 'Eléctrico', 'Fibra'],
-    datasets: [{
-        label: 'Valor total en inventario ($)',
-        data: [12300, 7450, 3800], // 🔹 Puedes cambiar estos valores dinámicamente desde tu base de datos
-        backgroundColor: ['#D31813', '#E4B9B8', '#CD908F'],
-        hoverOffset: 10,
-        borderRadius: 8,
-        borderWidth: 0
-    }]
-};
-
-new Chart(ctx, {
-    type: 'bar',
-    data: data,
-    options: {
-        responsive: true,
-        plugins: {
-            legend: { display: false },
-            tooltip: {
-                backgroundColor: '#fff',
-                titleColor: '#0D0503',
-                bodyColor: '#0D0503',
-                borderColor: '#CD908F',
-                borderWidth: 1
-            }
-            
-        },
-        cutout: '70%',
-    }
-});
